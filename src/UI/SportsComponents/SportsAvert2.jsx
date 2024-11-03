286212c36ebbaa48533a15108f59a8f0dd380d5d
chidera-collins <chideracollins24@gmail.comm>
2024-11-03 23:53:10 +0100

    Dera Push

---
+++ src/UI/SportsComponents/SportsAvert2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SportsAvert2() {
+  return (
+    <div className='min-h-[200px] bg-slate-300'>
+        PLACE AN ADVERT HERE
+    </div>
+  )
+}
+
+export default SportsAvert2
--- src/UI/SportsComponents/SportsComponent.jsx
+++ src/UI/SportsComponents/SportsComponent.jsx
@@ -4,6 +4,11 @@ import SportsComponent2 from './SportsComponent2'
 import SportsAdvert1 from './SportsAdvert1'
 import SportsComponent3 from './SportsComponent3'
 import SportsComponent4 from './SportsComponent4'
+import SportsComponent5 from './SportsComponent5'
+import SportsAvert2 from './SportsAvert2'
+import SportsComponent6 from './SportsComponent6'
+import SportsComponent7 from './SportsComponent7'
+import SportsComponent8 from './SportsComponent8'
 
 function SportsComponent() {
   return (
@@ -13,6 +18,11 @@ function SportsComponent() {
       <SportsAdvert1/>
       <SportsComponent3/>
       <SportsComponent4/>
+      <SportsComponent5/>
+      <SportsAvert2/>
+      <SportsComponent6/>
+      <SportsComponent7/>
+      <SportsComponent8/>
     </div>
   )
 }
