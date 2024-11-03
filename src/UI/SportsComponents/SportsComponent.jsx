21c81c470b75df087ba9f94d9010ce5cbbd12a2a
chidera-collins <chideracollins24@gmail.comm>
2024-11-03 02:25:31 +0100

    Collins Push

--- src/UI/SportsComponents/SportsComponent.jsx
+++ src/UI/SportsComponents/SportsComponent.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import SportsComponent1 from './SportsComponent1'
+import SportsComponent2 from './SportsComponent2'
 
 function SportsComponent() {
   return (
-    <div>SportsComponent</div>
+    <div  className=''>
+      <SportsComponent1/>
+      <SportsComponent2/>
+    </div>
   )
 }
 
---
+++ src/UI/SportsComponents/SportsComponent2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SportsComponent2() {
+  return (
+    <div>SportsComponent2</div>
+  )
+}
+
+export default SportsComponent2
