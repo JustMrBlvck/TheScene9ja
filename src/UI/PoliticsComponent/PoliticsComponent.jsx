d31f8a8665ccbc479d00fef84dd97d48449dbf26
chidera-collins <chideracollins24@gmail.comm>
2024-11-16 16:25:23 +0100

    dera

---
+++ src/UI/PoliticsComponent/PoliticsComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function PoliticsComponent() {
+  return (
+    <div className='h-[400px] bg-red-500'>PoliticsComponent</div>
+  )
+}
+
+export default PoliticsComponent
