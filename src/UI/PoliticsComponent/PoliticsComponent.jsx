9d5e7942e8541bdd781cc496dba1e07bbac93e93
chidera-collins <chideracollins24@gmail.comm>
2024-11-18 12:06:31 +0100

    dera codes

--- src/UI/PoliticsComponent/PoliticsComponent.jsx
+++ src/UI/PoliticsComponent/PoliticsComponent.jsx
@@ -1,12 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-
-
-function PoliticsComponent() {
-  return (
-    <div className='h-[400px] bg-red-500'>PoliticsComponent</div>
-=======
 import PoliticsFirstComponent from './PoliticsFirstComponent'
 
 function PoliticsComponent() {
@@ -16,7 +8,6 @@ function PoliticsComponent() {
            <PoliticsFirstComponent/>
 
     </div>
->>>>>>> d46ce0c27dd6e0275f8d2578f639b13c11515e5c
   )
 }
 
