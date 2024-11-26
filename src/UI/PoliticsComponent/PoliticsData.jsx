3b9c5fe6f028051e88a7ab38f16e7c975638b155
chidera-collins <chideracollins24@gmail.comm>
2024-11-26 14:49:10 +0100

    dera installed diasy ui

---
+++ src/UI/PoliticsComponent/PoliticsData.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function PoliticsData() {
+    const politicsNews=[
+        {
+            img:'https://www.channelstv.com/2024/11/26/ebonyi-gov-suspends-two-commissioners-others-over-alleged-misconduct/',
+            time:'2h',
+            author:'opeluwami akintayo',
+            heading:'Ebonyi gov suspends two commisioners, others over alleged misconduct',
+            about:'He directed the indefinite suspension of the Commissioner for Housing and Urban Development, Engineer Francis Ori, and a three-month suspension for his counterpart Dr. Moses Ekuma, Commissioner for Health and Social Development.',
+        },
+        {
+            img:'https://www.channelstv.com/2024/11/26/data-breach-gov-okpebholo-orders-urgent-review-of-edo-e-governance-platform/',
+            time:'4h',
+            author:'opeluwami akintayo',
+            heading:'Ebonyi gov suspends two commisioners, others over alleged misconduct',
+            about:'Data Breach: Gov Okpebholo Orders Urgent Review Of E-Governance Platform',
+
+        },
+        {
+            img:'https://www.channelstv.com/2024/11/25/nigeria-would-probably-have-been-better-if-peter-obi-won-2023-election-sen-abaribe/',
+            time:'14h',
+            author:'donatus anuchebe',
+            heading:'Nigeria Would Probably Have Been Better If Peter Obi Won 2023 Election - Sen Abaribe'
+
+        },
+        {
+            img:'https://www.channelstv.com/2024/11/25/almost-500-edo-govt-vehicles-still-missing-says-okpebholos-spokesman/',
+            time:'22h',
+            author:'emmanuel egbommandu',
+            heading:'Almost 500 Edo Govt Vehicles Still Missing, Says Okpebholo’s Spokesman'
+
+        },
+        {
+            img:'https://www.channelstv.com/2024/11/25/nimet-governors-to-sign-mou-on-climate-change/',
+            time:'1d',
+            author:'opelumai akintayo',
+            heading:'NiMet, Governors To Sign MoU On Climate Change'
+        },
+    ]
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PoliticsData
