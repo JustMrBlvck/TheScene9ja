c5e05cd836a4930067685b98127470c98a940d6e
chidera-collins <chideracollins24@gmail.comm>
2024-12-03 02:59:08 +0100

    Dera Push For Entertainment Folder

--- src/UI/EntertainmentComponent.jsx/EntertainmentData.jsx
+++ src/UI/EntertainmentComponent.jsx/EntertainmentData.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EntertainmentFirstComponent from './EntertainmentFirstComponent'
 
 function EntertainmentData() {
@@ -9,11 +9,30 @@ function EntertainmentData() {
             author:'Emmanuel EgboBiamadu',
             heading:"‘I Could Not Sit Up,’ Omotola Narrates Ordeal With Life-Threatening Sickness",
             about:'The 46-year-old said she was rushed to the emergency unit several times during the illness.'
-        }
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Omotola-Jalade-Ekehinde.jpg',
+            time:'15h',
+            author:'Emmanuel EgboBiamadu',
+            heading:"‘I Could Not Sit Up,’ Omotola Narrates Ordeal With Life-Threatening Sickness",
+            about:'The 46-year-old said she was rushed to the emergency unit several times during the illness.'
+        },
 
     ]
       
     const subEntertainment=[
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/WhatsApp-Image-2024-11-26-at-5.17.42-PM.jpeg',
+            time:'3d',
+            author:'Emmanuel  Egbobiamadu',
+            heading:"Nigeria Is The Cultural Capital Of The World – Wanle Akinboboye"
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/AFP__20241125__36N89F2__v1__Preview__OpeningNightOfLaCageAuxFolles.jpg',
+            time:'3d',
+            heading:"Bestselling ‘Woman Of Substance’ Author Barbara Taylor Bradford Dies At 91",
+            author:'Khadijat lawal'
+        },
         {
             img:'https://www.channelstv.com/wp-content/uploads/2024/11/WhatsApp-Image-2024-11-26-at-5.17.42-PM.jpeg',
             time:'3d',
@@ -39,16 +58,132 @@ function EntertainmentData() {
             time:'3d',
             author:'Khadijat lawal',
             heading:'Biggest Predictions For The 67th Annual Grammy Awards'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Grammy-Awards.jpg',
+            time:'3d',
+            author:'Khadijat lawal',
+            heading:'Biggest Predictions For The 67th Annual Grammy Awards'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/09/EMMYS.jpg',
+            time:'3d',
+            author:'khadijat lawal',
+            heading:"Manga Adaptation ‘Drops Of God’ Nets International Emmy Award"
         }
     ]
-     
-    
+
+    const mainEntertainment = [
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/phyna-BBNaija-2.jpg',
+            heading:'BBN’s Phyna Sent Out From Restaurant Over Indecent Dressing'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/09/Basketmouth-Victoria-Eze.jpg',
+            heading:'Basketmouth’s First Feature Film ‘A Ghetto Love Story’ To Hit Cinemas Nov 22'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/AFP__20241121__36MU29V__v1__Preview__FilesFranceMediaInternetCinemaBook.jpg',
+            heading:'French YouTuber Takes On Manga After Conquering Everest'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/AFP__20241121__36MY7LA__v1__Preview.jpg',
+            heading:'Banana Taped To A Wall Sells For $6.2 Million In New York',
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Rust-Movie-Premiere.jpg',
+            heading:'Alec Baldwin’s ‘Rust’ Premieres Three Years After On-Set Shooting'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Barry-Jenkins-1.jpg',
+            heading:'VIDEO: ‘Mufasa’ Will Be First Experience Of Africa For Some People, Says Director'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/AFP__20241120__36MW8Q8__v1__Preview__PolandUsEntertainmentFilm.jpg',
+            heading:'‘Rust’ To premiere At Polish Film Festival Three Years After On-Set Shooting'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Denzel_Washington-1.jpg',
+            heading:'Denzel Washington Confirms Two More Equaliser Movies'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Renes-Painting.jpg',
+            heading:'Rene Magritte’s Painting Sells For Record $121 Million'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/AFP__20241121__36MU29V__v1__Preview__FilesFranceMediaInternetCinemaBook.jpg',
+            heading:'French YouTuber Takes On Manga After Conquering Everest'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/AFP__20241121__36MY7LA__v1__Preview.jpg',
+            heading:'Banana Taped To A Wall Sells For $6.2 Million In New York',
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Rust-Movie-Premiere.jpg',
+            heading:'Alec Baldwin’s ‘Rust’ Premieres Three Years After On-Set Shooting'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Barry-Jenkins-1.jpg',
+            heading:'VIDEO: ‘Mufasa’ Will Be First Experience Of Africa For Some People, Says Director'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/AFP__20241120__36MW8Q8__v1__Preview__PolandUsEntertainmentFilm.jpg',
+            heading:'‘Rust’ To premiere At Polish Film Festival Three Years After On-Set Shooting'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Denzel_Washington-1.jpg',
+            heading:'Denzel Washington Confirms Two More Equaliser Movies'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/Renes-Painting.jpg',
+            heading:'Rene Magritte’s Painting Sells For Record $121 Million'
+        },
+        {
+            img:'https://www.channelstv.com/wp-content/uploads/2024/11/phyna-BBNaija-2.jpg',
+            heading:'BBN’s Phyna Sent Out From Restaurant Over Indecent Dressing'
+        }
+    ]
+
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerHeadPage=1
+    const itemsPerPage = 2;
+    const itemsPerPageMain=9;
+
+    function paginateArray(array , itemsPerPage) {
+        const lastPostIndex = currentPage * itemsPerPage;
+        const firstPostIndex = lastPostIndex - itemsPerPage;
+       return array.slice(firstPostIndex, lastPostIndex);
+        
+    }
+    const paginateHeadEntertainment = paginateArray(headEntertainment, itemsPerHeadPage);
+    const paginateSubEntertainment= paginateArray(subEntertainment,itemsPerPage);
+    const paginateAsideEntertainment = paginateArray(asideEntertainment,itemsPerPage);
+    const paginateMainEntertainment = paginateArray(mainEntertainment,itemsPerPageMain);
+
+    const totalItems = Math.max(
+        Math.ceil(headEntertainment.length/itemsPerHeadPage),
+        subEntertainment.length,
+        asideEntertainment.length,
+        Math.ceil(mainEntertainment.length/itemsPerPageMain)*itemsPerPage
+    )
+
+    const totalPages =Math.ceil(totalItems/itemsPerPage)
+
+    const paginate=(pageNumber)=>setCurrentPage(pageNumber)
+
+
+
   return (
     <div>
         <EntertainmentFirstComponent
-        headEntertainment={headEntertainment}
-        subEntertainment={subEntertainment}
-        asideEntertainment={asideEntertainment}
+        headEntertainment={paginateHeadEntertainment}
+        subEntertainment={paginateSubEntertainment}
+        asideEntertainment={paginateAsideEntertainment}
+        mainEntertainment={paginateMainEntertainment}
+        paginate={paginate}
+        totalPages={totalPages}
+        currentPage={currentPage}
 
         />
     </div>
--- src/UI/EntertainmentComponent.jsx/EntertainmentFirstComponent.jsx
+++ src/UI/EntertainmentComponent.jsx/EntertainmentFirstComponent.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function EntertainmentFirstComponent({headEntertainment=[], subEntertainment=[],asideEntertainment=[]}) {
+function EntertainmentFirstComponent({
+  headEntertainment=[], 
+  subEntertainment=[],
+  asideEntertainment=[],
+  mainEntertainment=[],
+  paginate,
+  currentPage,
+  totalPages
+}) {
+
+            {/* CREATING AN OBJECT FOR ADVERT JUST FOR AN EASY ACCESS HERE  */}
+  const advertBox = {
+    AD: "https://dsp-media.eskimi.com/upload/142402_2123234271_0bd86081fa39063dcaeca9893dde821d.jpg",
+   ADd: "https://sunrise.ng/wp-content/uploads/2024/04/RSA-Mortgage-digital-banner-300x250-1.png"
+ }
+ 
   return (
     <div >
       <div className='min-h-[100px] dark:text-white flex items-center flex-col'>
@@ -61,18 +76,64 @@ function EntertainmentFirstComponent({headEntertainment=[], subEntertainment=[],
             ))}
 
           </div>
-          <div className='flex md:col-span-2 lg:justify-end  flex-col md:grid md:grid-cols-2 lg:mt-[-200px]  w-full lg:w-[50%] lg:ml-[101%]  gap-2 bg-yellow-200'>
+          <div className='flex md:col-span-2 lg:justify-end  flex-col md:grid md:grid-cols-2 lg:mt-[-200px]  w-full lg:w-[50%] lg:ml-[101%]  gap-2 '>
             {asideEntertainment.map((items,index)=>(
              <Link key={index}>
                <div>
-                <img src={items.img} alt={items.about} />
+                <img src={items.img} alt={items.heading} />
+              </div>
+              <div className='flex gap-1 items-center dark:text-white'>
+                  <h1 className='before:w-[10px] before:h-[10px] flex relative items-center before:bg-[red]'>{items.time} |</h1>
+                  <h1 className=' font-Custom font-bold'>{items.author}</h1>
+              </div>
+              <div className='font-Custom font-bold'>
+                <h3>{items.heading}</h3>
               </div>
              
              </Link>
 
             ))}
+          </div>
+
+        </div>
+        <div className='min-h-[100px] w-full md:w-[97%] lg:w-[90%]  grid grid-cols-1 md:grid-cols-1 lg:grid-cols-[80%,20%] mt-4 gap-2'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+              {mainEntertainment.map((item,index)=>(
+                <Link key={index}>
+                  <div>
+                    <img src={item.img} height='100%' width='100%' loading='lazy' alt={item.heading} />
+                  </div>
+                  <div className='font-Custom font-bold text-[1.2rem]'>
+                    <h3>{item.heading}</h3>
+                  </div>
+
+                </Link>
+              ))}
 
           </div>
+        {/* <div className='min-h-[100px] bg-yellow-200 w-full lg:hidden '>
+          wsdcfv 
+
+        </div> */}
+      
+          <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 '>
+              {Object.values(advertBox).map((url, index) => (
+                    <img key={index} src={url} alt={`Advert ${index}`} />
+                  ))}       
+          </div>
+    
+        </div>
+        <div className='min-h-[40px] w-full md:w-[96%] lg:w-[90%]  mt-2'>
+          {Array.from({length:totalPages},(_, i)=>(
+             <button
+             key={i + 1}
+             className={`btn ${currentPage === i + 1 ? "btn-active" : ""}`}
+             onClick={() => paginate(i + 1)}
+           >
+             {i + 1}
+           </button>
+          ))}
+           
 
         </div>
 
